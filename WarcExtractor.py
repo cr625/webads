025aa8fbbb659a6408fa8df0b0c657a34d03f768
cr625 <cr625@drexel.edu>
2022-12-27 14:39:37 -0500

    get all captures for a url for a collection

--- WarcExtractor.py
+++ WarcExtractor.py
@@ -1,13 +1,25 @@
 from warcio.archiveiterator import ArchiveIterator
 import boto3
 from io import BytesIO
+import requests
+import json
+
+default_collection = "CC-MAIN-2022-40"
 
 class WarcExtractor(object):
     """
     Extracts WARC records from a warc.gz file."""
     
-    def __init__(self):
+    def __init__(self, target_url):
         self.s3client = boto3.client('s3')
+        self.target_url = target_url
+        self.collection = default_collection
+
+    def get_index(self):
+        cdx_server = "https://index.commoncrawl.org/{collection}-index?url={url}&matchType=domain&output=json"
+        cdx_response = requests.get(cdx_server.format(collection=self.collection, url=self.target_url)).content.splitlines()
+        return [json.loads(line) for line in cdx_response]
+        
 
     
     def get_record(self, offset, length, warc_file):
@@ -19,13 +31,14 @@ class WarcExtractor(object):
             page = record.content_stream().read()            
             yield page
 
-offset = 1124184665
-length = 64704
-url = "https://www.usatoday.com/"
-warc_file = "crawl-data/CC-MAIN-2022-40/segments/1664030333541.98/warc/CC-MAIN-20220924213650-20220925003650-00631.warc.gz"
+if __name__ == "__main__":
+
+    we = WarcExtractor('https://www.usatoday.com/')
+    index = we.get_index()
+
+    for entry in index:
+        print(entry['offset'], entry['length'], entry['filename'])
 
 
-we = WarcExtractor()
-pages = we.get_record(offset, length, warc_file)
-for page in pages:
-    print(page)
+    
+    
